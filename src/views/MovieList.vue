975ce7c21328126996906c7ebd7efaecebf365b4
obscureguess <obscureguess@gmail.com>
2020-04-25 13:17:14 +0300

    style fix

--- src/views/MovieList.vue
+++ src/views/MovieList.vue
@@ -109,8 +109,9 @@ export default {
 }
 .spinner {
   color: #fff;
-  display: flex;
-  justify-content: center;
-  margin-top: 20px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
 }
 </style>
